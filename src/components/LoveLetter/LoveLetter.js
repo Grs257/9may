ad72b0106d551ffc010d670a62abe0c9718fd8a1
BODDU GIRIRAJASEKHAR <116698450+Grs257@users.noreply.github.com>
2024-05-08 09:18:42 +0530

    added

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./LoveLetter.css";
 import audioFile1 from "./1.mp3";
-import audioFile2 from "./2.mp3";
-import audioFile3 from "./3.mp3";
+import audioFile2 from "./song3.mp3";
+import audioFile3 from "./song2.mp3";
 import background1 from "./BH1.jpg";
 import background2 from "./BH2.jpg";
 import background3 from "./BH3.jpg";
 import bg from "./BG.jpg";
+import bg2 from "./BG2.jpg";
+import bg3 from "./BG3.jpg";
 
 const backgrounds = [background1, background2, background3];
 const emojis = ["❤️", "😍", "💖", "😘"];
@@ -19,35 +21,78 @@ const envelopeData = [
     animationClass: "animation1",
     content: (
       <div>
-        <p>Happy Birthday, Bhama 💖,</p>
-        <p>May this year bring you loads of happiness and joy.</p>
-        <p>Love, Your Forever.</p>
+        <p>
+          Nenu naa childhood lo edhaina unte bagundedhi ani anukune vishyam ento
+          telsa adhi nee childhood birthdays appudaithey nee cute cute face
+          chusthu😁,
+        </p>
+        <p>
+          Happy Birthday to you ani cheppi happy ayye vadni kadha ani
+          alochistha.
+        </p>
+        <p>
+          Ponile mana pelli nundi prathi birthday full joy ga celebrate
+          cheskundham
+        </p>
+        <p>
+          <strong color="Red">Love you baby😘😘😘</strong>
+          <br />
+          with love, nee childhood miss aina pilladni ❤️
+        </p>
       </div>
     ),
   },
   {
     id: 2,
     audio: audioFile2,
-    background: bg,
+    background: bg2,
     animationClass: "animation2",
     content: (
       <div>
-        <p>Dearest Bhama,</p>
-        <p>Wishing you all the success in the world in your new endeavors.</p>
-        <p>Stay blessed, Your Buddy.</p>
+        <p>
+          Mana Vayasulo unnappudu mana cheyyavalsina konni badhyatalu mana medha
+          untai ala ani manaki okari medha prema inkokari taggipothundhi ani
+          kadhu chupinchukune time undadhu anthe
+        </p>
+        <p>
+          Mana pillalu, mana parents,mana works,mana idealogy share
+          cheskovadam,chinna godavalu aa taravata bujjaginchukovadam ila anni ee
+          phase lone untai so chala patentience avasaram naku unnantha patience
+          lekapoina parledhu nuv kuda handle cheyyagalav ani nammuthunna 😉😅😂
+        </p>
+        <p>
+          <strong color="Red">Love you Pellam😘😘😘</strong>
+          <br />
+          Itlu, Prema tho nee mogudu ❤️
+        </p>
       </div>
     ),
   },
   {
     id: 3,
     audio: audioFile3,
-    background: bg,
+    background: bg3,
     animationClass: "animation3",
     content: (
       <div>
-        <p>Hello Bhama!</p>
-        <p>Let's make this birthday the beginning of an adventurous year.</p>
-        <p>Yours adventurously, The Explorer.</p>
+        <p>
+          Ee phase lo full time untundhi ika mana istam anuko prathi roju mana
+          nacchina time ki lechi ala oka morning walk,sayanthram oka evening
+          walk prathi vishyam lo manaki prapancham tho pani lekunda nuvvu nenu
+          anthe, challani sayantralalo chakkaga comfort unna chairs pakka
+          pakkana veskuni manam appati varaku gadipina moments ni share
+          cheskuntu blush avthu cherish aavdham ematav bokkipallu pellama🤣
+        </p>
+        <p>
+          Manam musali ayyaka nee palla settu dachesi ata pattistha 🤣 nuv
+          cheppindhi naku vinapadadhu em antunnave tingari ani vekkiristhu aa
+          sayantralu netho gadipestha 😘😘😘
+        </p>
+        <p>
+          <strong color="Red">Love you bokkipallu musali😘😘😘</strong>
+          <br />
+          Itlu, Prema tho nee sound engineer musalodu ❤️
+        </p>
       </div>
     ),
   },
